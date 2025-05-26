a0ab0e77aa7141ec43077a2adcf6cbd601ca55bc
phil-pk-hobbies <phil@pk-hobbies.com>
2025-05-25 22:43:54 -0400

    feat: restore assistant list layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreateAssistantPage from './pages/CreateAssistantPage';
 import EditAssistantPage from './pages/EditAssistantPage';
+import ChatPage from './pages/ChatPage';
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/assistants/new" element={<CreateAssistantPage />} />
       <Route path="/assistants/:id/edit" element={<EditAssistantPage />} />
-      <Route path="/assistants/:id" element={<HomePage />} />
+      <Route path="/assistants/:id" element={<ChatPage />} />
     </Routes>
   );
 }
