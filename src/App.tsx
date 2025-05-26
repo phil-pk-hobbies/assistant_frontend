836ac6670dca0f30f8aef06c7ac7688db0a529bf
phil-pk-hobbies <phil@pk-hobbies.com>
2025-05-25 21:04:46 -0400

    Add edit assistant feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreateAssistantPage from './pages/CreateAssistantPage';
+import EditAssistantPage from './pages/EditAssistantPage';
 import ChatPage from './pages/ChatPage';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/assistants/new" element={<CreateAssistantPage />} />
+      <Route path="/assistants/:id/edit" element={<EditAssistantPage />} />
       <Route path="/assistants/:id" element={<ChatPage />} />
     </Routes>
   );
