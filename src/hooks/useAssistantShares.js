3f6a12dbcd9fd7ba90fc6daa1c810e6713fc698f
phil-pk-hobbies <phil@pk-hobbies.com>
2025-05-27 15:47:32 -0400

    fix share hooks

--- src/hooks/useAssistantShares.js
+++ src/hooks/useAssistantShares.js
@@ -1,23 +1,25 @@
 import { useQuery } from '@tanstack/react-query';
 import api from '../api/axios';
 
-export function useAssistantUserShares(aid) {
+export function useAssistantUserShares(aid, options = {}) {
   return useQuery({
     queryKey: ['assistant', 'shares', aid, 'users'],
     queryFn: async () => {
       const { data } = await api.get(`/api/assistants/${aid}/shares/users/`);
       return data;
     },
+    ...options,
   });
 }
 
-export function useAssistantDeptShares(aid) {
+export function useAssistantDeptShares(aid, options = {}) {
   return useQuery({
     queryKey: ['assistant', 'shares', aid, 'depts'],
     queryFn: async () => {
       const { data } = await api.get(`/api/assistants/${aid}/shares/departments/`);
       return data;
     },
+    ...options,
   });
 }
 
